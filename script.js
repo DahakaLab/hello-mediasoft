b73b0d0fe6843b170e782f7b3cbe5b9ca8b94625
dahakalab <dahakalab@gmail.com>
2019-07-03 00:21:59 +0300

    Creating calculator

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-alert('Hello Mediasoft!');
+const messageOutEl = document.querySelector('h1');
+const inputStartBtn = document.querySelector('button');
+
+const changeMessage = (message) => {
+  messageOutEl.innerText = message;
+};
+
+const calculateExpression = (expression) => {
+  try {
+    changeMessage(`Результат:\n${expression} = ${eval(expression)}`);
+  } catch {
+    changeMessage(`Выражение \'${expression}\' неккоректно`);
+  }
+};
+
+const checkExpression = (expression) => {
+  const validCharacters = /[^0-9\+\-\*\/\(\)\ ]/;
+  if (!validCharacters.test(expression)) {
+    calculateExpression(expression);
+  } else {
+    changeMessage('Используйте только числа и допустимые символы: (\'+\', \'-\', \'*\', \'/\', \' \').');
+  }
+};
+
+const inputStart = () => {
+  const inputValue = prompt('Введите вычисляемое выражение', '');
+  if (inputValue) {
+    checkExpression(inputValue);
+  } else {
+    changeMessage('Ввод отменен');
+  }
+};
+
+inputStartBtn.addEventListener('click', () => inputStart());
