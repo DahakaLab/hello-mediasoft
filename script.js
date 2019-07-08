b885d90d589a4c0bcb3a0c0a4c9c0fd3ce429202
dahakalab <dahakalab@gmail.com>
2019-07-08 10:48:38 +0300

    Adding comments into file script

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const messageOutEl = document.querySelector('h1');
-const inputStartBtn = document.querySelector('button');
+const messageOutEl = document.querySelector('h1'); // Поиск элемента в документе для вставки сообщения.
+const inputStartBtn = document.querySelector('button'); // Поиск кнопки по клику которой открывается окно с полем для ввода выражения.
 
-const calculator = {
-  createResultMessage(expression) {
+const calculator = { // объект калькулятора где описана вся логика проверок, вычислений выражения и подготовка финального сообщения.
+  createResultMessage(expression) { // Публичный метод который возвращает финальное сообщение.
     const errors = this._checkErrors.resultErrors(expression);
     if (errors.length) {
       return errors.join('\n');
@@ -21,14 +21,13 @@ const calculator = {
           return `Выражение \'${expression}\' некорректно`;
         }
       } catch(e) {
-        console.log(e);
         return `Выражение \'${expression}\' некорректно`;
       }
     }
   },
-  _checkErrors: {
-    _validCharactersRegExp: /[^0-9\+\-\*\/\(\)\ \.]/,
-    resultErrors(expression) {
+  _checkErrors: { // Приватная абстракция калькулятора, где описана все логика проверок корректности выражения. Возвращает массив ошибок. 
+    _validCharactersRegExp: /[^0-9\+\-\*\/\(\)\ \.]/, // Приватное свойство проверки, где описаны допустимые символы.
+    resultErrors(expression) { // Публичный метод проверки, возврщает массив ошибок.
       const errors = [];
       const resultValidCharacters = this._validCharacters(expression);
       if (resultValidCharacters) errors.push(resultValidCharacters);
@@ -38,12 +37,12 @@ const calculator = {
       
       return errors;
     },
-    _validCharacters(expression) {
+    _validCharacters(expression) { // Приватный метод проверки, возвращает сообщение с ошибкой если выражение содержит недопустимые символы
       if (this._validCharactersRegExp.test(expression)) {
         return 'Используйте только числа и допустимые символы: (\'+\', \'-\', \'*\', \'/\', \' \', \'.\').';
       }
     },
-    _countBrackets(expression) {
+    _countBrackets(expression) { // Приватный метод проверки, возвращает сообщение с ошибкой если выражение имеет разное количество открывающих и закрывающих скобок.
       const openBrackets = expression.match(/\(/g) ? expression.match(/\(/g).length : 0;
       const closeBrackets = expression.match(/\)/g) ? expression.match(/\)/g).length : 0;
       if (openBrackets !== closeBrackets) {
@@ -51,22 +50,21 @@ const calculator = {
       }
     }
   },
-  _clearingExpression(expression) {
+  _clearingExpression(expression) { // Приватный метод калькулятора, возвращает выражение без пробелов.
     return expression.replace(/\s/g, '');
   },
-  _calculate: {
-    simplifiedExpression: [],
-    resultsExpression: [],
-    resultExpression(expression) {
+  _calculate: { // Приватная абстракция калькулятора, где описана вся логика вычисления вычисления.
+    simplifiedExpression: [], // Публичное свойство расчета, куда записыватся упрощенное выражение после раскрытия скобок.
+    resultsExpression: [], // Публичное свойство расчета, куда записываются результаты каждой операции выражения.
+    resultExpression(expression) { // Публичный метод расчета, который возвращает расчет и производит запись в свойства simplifiedExpression и resultsExpression.
       this.simplifiedExpression = this._openingBrackets(expression);
       this.resultsExpression = this._calculationExpression(this.simplifiedExpression);
       return this.resultsExpression[this.resultsExpression.length - 1];
     },
-    _calculationOperation(expression) {
-      // TODO: Написать алгоритм расчета операции
+    _calculationOperation(expression) { // Приватный метод расчета, которое возвращает результат принятого выражения.
       return eval(expression);
     },
-    _openingBrackets(expression, simplifiedExpressions = []) {
+    _openingBrackets(expression, simplifiedExpressions = []) { // Приватный метод расчета. Рекурсивное раскрытие скобок, котрое возвращает массив пошаговых операций если скобок не осталось.
       const indexTo = expression.indexOf(')');
       if(~indexTo) {
         let indexFrom = expression.indexOf('(');
@@ -86,7 +84,7 @@ const calculator = {
         return simplifiedExpressions;
       }
     },
-    _calculationExpression(simplifiedExpression) {
+    _calculationExpression(simplifiedExpression) { // Приватный метод расчета, возвращает массив результатов пошаговых операций.
       const resultsSimplifiedExpression = [];
       simplifiedExpression.forEach((operation) => {
         const finalOperation = this._substitutionValue(resultsSimplifiedExpression, operation);
@@ -94,7 +92,7 @@ const calculator = {
       });
       return resultsSimplifiedExpression;
     },
-    _substitutionValue(resultsSimplifiedExpression, operation) {
+    _substitutionValue(resultsSimplifiedExpression, operation) { // Приватный метод расчета. Рекурсивная подстановка значений вместо переменных. Возвращает конечное выражение.
       const entryPoint = operation.indexOf('x');
       if (~entryPoint) {
         const targetOperationIndex = parseInt(operation.charAt(entryPoint + 1), 10);
@@ -107,11 +105,11 @@ const calculator = {
   },
 };
 
-const changeMessage = (message) => {
+const changeMessage = (message) => { // Функция вставки текста в элемент документа.
   messageOutEl.innerText = message;
 };
 
-const inputStart = () => {
+const inputStart = () => { // Функция получения значения и запуск метода получения финального сообщения после расчета выражения в калькуляторе.
   const expression = prompt('Введите вычисляемое выражение', '');
   if (expression) {
     changeMessage(calculator.createResultMessage(expression));
@@ -120,4 +118,4 @@ const inputStart = () => {
   }
 };
 
-inputStartBtn.addEventListener('click', () => inputStart());
+inputStartBtn.addEventListener('click', () => inputStart()); // По клику на кнопку запусе функции получния значения.
