1ad4f4835114970d460d5a139bbd813870c51c1f
dahakalab <dahakalab@gmail.com>
2019-07-16 03:32:45 +0300

    Resolve 1 task

--- script.js
+++ script.js
@@ -1,121 +1,153 @@
 const messageOutEl = document.querySelector('h1'); // Поиск элемента в документе для вставки сообщения.
 const inputStartBtn = document.querySelector('button'); // Поиск кнопки по клику которой открывается окно с полем для ввода выражения.
 
-const calculator = { // объект калькулятора где описана вся логика проверок, вычислений выражения и подготовка финального сообщения.
-  createResultMessage(expression) { // Публичный метод калькулятора который возвращает финальное сообщение.
-    const errors = this._checkErrors.resultErrors(expression);
-    if (errors.length) {
-      return errors.join('\n');
-    } else {
-      try {
-        const resultExpression = this._calculate.resultExpression(this._clearingExpression(expression));
-        if (resultExpression !== undefined) {
-          let solutionMessage = 'Решение:\n';
-          const resultMessage = `Результат:\n${expression} = ${resultExpression}`;
-          const resultsExpression = this._calculate.resultsExpression;
-          this._calculate.simplifiedExpression.forEach((expression, index) => {
-            solutionMessage = `${solutionMessage}x${index}: ${expression} = ${resultsExpression[index]};\n`;
-          });
-          return `${solutionMessage}\n${resultMessage}`;
-        } else {
-          return `Выражение \'${expression}\' некорректно`;
-        }
-      } catch(e) {
-        return `Выражение \'${expression}\' некорректно`;
-      }
-    }
-  },
-  _checkErrors: { // Приватная абстракция калькулятора, где описана все логика проверок корректности выражения. Возвращает массив ошибок. 
-    _validCharactersRegExp: /[^0-9\+\-\*\/\(\)\ \.]/, // Приватное свойство проверки, где описаны допустимые символы.
-    resultErrors(expression) { // Публичный метод проверки, возврщает массив ошибок.
-      const errors = [];
-      const resultValidCharacters = this._validCharacters(expression);
-      if (resultValidCharacters) errors.push(resultValidCharacters);
-
-      const resultCountBrackers = this._countBrackets(expression);
-      if (resultCountBrackers) errors.push(resultCountBrackers);
-      
-      return errors;
-    },
-    _validCharacters(expression) { // Приватный метод проверки, возвращает сообщение с ошибкой если выражение содержит недопустимые символы
-      if (this._validCharactersRegExp.test(expression)) {
-        return 'Используйте только числа и допустимые символы: (\'+\', \'-\', \'*\', \'/\', \' \', \'.\').';
-      }
-    },
-    _countBrackets(expression) { // Приватный метод проверки, возвращает сообщение с ошибкой если выражение имеет разное количество открывающих и закрывающих скобок.
-      const openBrackets = expression.match(/\(/g) ? expression.match(/\(/g).length : 0;
-      const closeBrackets = expression.match(/\)/g) ? expression.match(/\)/g).length : 0;
-      if (openBrackets !== closeBrackets) {
-        return 'Количество открывающих и закрывающих скобок должно быть одинаковым.';
-      }
-    }
-  },
-  _clearingExpression(expression) { // Приватный метод калькулятора, возвращает выражение без пробелов.
-    return expression.replace(/\s/g, '');
-  },
-  _calculate: { // Приватная абстракция калькулятора, где описана вся логика вычисления вычисления.
-    simplifiedExpression: [], // Публичное свойство расчета, куда записыватся упрощенное выражение после раскрытия скобок.
-    resultsExpression: [], // Публичное свойство расчета, куда записываются результаты каждой операции выражения.
-    resultExpression(expression) { // Публичный метод расчета, который возвращает расчет и производит запись в свойства simplifiedExpression и resultsExpression.
-      this.simplifiedExpression = this._openingBrackets(expression);
-      this.resultsExpression = this._calculationExpression(this.simplifiedExpression);
-      return this.resultsExpression[this.resultsExpression.length - 1];
-    },
-    _calculationOperation(expression) { // Приватный метод расчета, которое возвращает результат принятого выражения.
-      return eval(expression);
-    },
-    _openingBrackets(expression, simplifiedExpressions = []) { // Приватный метод расчета. Рекурсивное раскрытие скобок, котрое возвращает массив пошаговых операций если скобок не осталось.
-      const indexTo = expression.indexOf(')');
-      if(~indexTo) {
-        let indexFrom = expression.indexOf('(');
-        while (true) {
-          const currentIndexFrom = expression.indexOf('(', (indexFrom + 1));
-          if (~currentIndexFrom && currentIndexFrom < indexTo) {
-            indexFrom = currentIndexFrom;
-          } else {
-            break;
-          }
-        }
-        const currentExpression = `${expression.slice(0, indexFrom)}x${simplifiedExpressions.length}${expression.slice(indexTo + 1)}`;
-        simplifiedExpressions.push(expression.slice(indexFrom + 1, indexTo));
-        return this._openingBrackets(currentExpression, simplifiedExpressions);
-      } else {
-        simplifiedExpressions.push(expression);
-        return simplifiedExpressions;
-      }
-    },
-    _calculationExpression(simplifiedExpression) { // Приватный метод расчета, возвращает массив результатов пошаговых операций.
-      const resultsSimplifiedExpression = [];
-      simplifiedExpression.forEach((operation) => {
-        const finalOperation = this._substitutionValue(resultsSimplifiedExpression, operation);
-        resultsSimplifiedExpression.push(this._calculationOperation(finalOperation));
-      });
-      return resultsSimplifiedExpression;
-    },
-    _substitutionValue(resultsSimplifiedExpression, operation) { // Приватный метод расчета. Рекурсивная подстановка значений вместо переменных. Возвращает конечное выражение.
-      const entryPoint = operation.indexOf('x');
-      if (~entryPoint) {
-        const targetOperationIndex = parseInt(operation.charAt(entryPoint + 1), 10);
-        const currentOperation = `${operation.slice(0, entryPoint)}${resultsSimplifiedExpression[targetOperationIndex]}${operation.slice(entryPoint+2)}`;
-        return this._substitutionValue(resultsSimplifiedExpression, currentOperation);
-      } else {
-        return operation;
-      }
-    },
-  },
-};
-
 const changeMessage = (message) => { // Функция вставки текста в элемент документа.
   messageOutEl.innerText = message;
 };
 
-const inputStart = () => { // Функция получения значения и запуск метода получения финального сообщения после расчета выражения в калькуляторе.
-  const expression = prompt('Введите вычисляемое выражение', '');
-  if (expression) {
-    changeMessage(calculator.createResultMessage(expression));
-  } else {
-    changeMessage('Ввод отменен');
+class AddMessage { // Класс для добавления елементов. 
+  constructor(targetEl = document.body) {
+    this.targetEl = targetEl;
   }
+
+  heading(number = 2, message = '') { // Метод добавления заголовков
+    const correctNumberHeading = (number && number > 0 && number <= 6);
+    const heading = correctNumberHeading ? document.createElement(`h${number}`) : document.createElement('h2');
+    heading.innerHTML = correctNumberHeading ? message : 'Неккоректный номер заголовка';
+    this.targetEl.appendChild(heading);    
+  }
+}
+
+class addNode {
+  static option(select, value = '') {
+    const option = document.createElement('option');
+    option.value = value;
+    option.innerText = value;
+    select.appendChild(option);
+  }
+}
+
+class FilmsInfo {
+  constructor(filmsTarget = [], filmsInfoEl = document.body) {
+    this._filmsTarget = filmsTarget;
+    this._addingMessage = new AddMessage(filmsInfoEl);
+    this._state = this._filmsTarget.reduce((state, film) => {
+      if (film.title) {
+        state.films[film.title] = FilmsInfo._deleteKeys(film, 'title');
+        this._addingMessage.heading(2, `Фильм: ${film.title}`);
+      };
+      if (film.director && film.director.name) {
+        state.directors[film.director.name] = FilmsInfo._deleteKeys(film.director, 'name');
+        this._addingMessage.heading(3, `Режжисер: ${film.title}`);
+      }
+      if (film.actors) {
+        const currentActorsObj = FilmsInfo._conversionArrToObject(film.actors);
+        Object.assign(state.actors, currentActorsObj);
+        this._addingMessage.heading(5, `Актеры: ${Object.keys(currentActorsObj).join(', ')}`);
+      }
+      this._addingMessage.heading(2, '***');
+      return state;
+    }, {
+      actors: {},
+      directors: {},
+      films: {},
+    });
+  }
+
+  get directors() {
+    return this._state.directors;
+  }
+
+  get state() {
+    return this._state;
+  }
+
+  static _conversionArrToObject(arr) {
+    const currentObj = {};
+    arr.forEach((obj) => {
+      const key = obj.name || obj.title;
+      if (key) {
+        currentObj[key] = Object.assign({}, FilmsInfo._deleteKeys(obj, 'name', 'title'));
+      }
+    });
+    return currentObj;
+  }
+
+  static _deleteKeys(obj, ...keys) {
+    const currentObj = Object.assign({}, obj);
+    keys.forEach((key) => {
+      delete currentObj[key];
+    })
+    return currentObj;
+  }
+}
+
+const initSelects = (directors = {}, state) => {
+  const selectDirectorsEl = document.querySelector('[name = \'directors\']');
+  const selectOscarsCountEl = document.querySelector('[name = \'oscarCount\']');
+  const currentOscarsCountsVariations = [];
+  Object.keys(directors).forEach((director) => {
+    const currentOscarsCounts = +directors[director].oscarsCount || 0;
+    addNode.option(selectDirectorsEl, director);
+    if (!~currentOscarsCountsVariations.indexOf(currentOscarsCounts)) {
+      addNode.option(selectOscarsCountEl, directors[director].oscarsCount);
+      currentOscarsCountsVariations.push(currentOscarsCounts);
+    }
+  });
+  if (selectDirectorsEl.value && selectOscarsCountEl.value && state) {
+    averageAgeActors(state, selectDirectorsEl.value, selectOscarsCountEl.value);
+    Array.of(selectDirectorsEl, selectOscarsCountEl).forEach((select) => {
+      select.addEventListener('change', () => averageAgeActors(state, selectDirectorsEl.value, selectOscarsCountEl.value));
+    });
+  }
+}
+
+const averageAgeActors = (state, directorName = '', directorOscarsCount = 0) => {
+  const actorsAverageAgeEl = document.querySelector('.actors-average-age');
+  const { directors, actors, films } = state;
+  const validDirectors = [];
+  const validActors = [];
+  Object.keys(directors).forEach((currentDirectorName) => {
+    if (+directors[currentDirectorName].oscarsCount === +directorOscarsCount && currentDirectorName === directorName) {
+      validDirectors.push(currentDirectorName);
+    }
+  });
+  if (validDirectors.length) {
+    Object.keys(films).forEach((filmTitle) => {
+      if (films[filmTitle].director && films[filmTitle].actors && ~validDirectors.indexOf(films[filmTitle].director.name)) {
+        films[filmTitle].actors.forEach((currentActor) => {
+          if (currentActor.name) validActors.push(currentActor.name);
+        })
+      }
+    });
+    const averageAgeValidActors = validActors.reduce((maxAge, currentValidActiors) => {
+      const currentValidActiorsAge = actors[currentValidActiors].age || 0;
+      return maxAge + currentValidActiorsAge;
+    }, 0) / validActors.length;
+
+    console.log(`Средний возраст актеров: ${+averageAgeValidActors.toFixed(2)}`);
+    actorsAverageAgeEl.innerHTML = `Средний возраст актеров: ${+averageAgeValidActors.toFixed(2)}`;
+  } else {
+    actorsAverageAgeEl.innerHTML = 'Нет такого режиссера с таким количеством оскара';
+  }
+}
+
+const loadSuccess = (filmsTarget) => {
+  const filmsInfoEl = document.querySelector('.films-info'); // Поиск елемента для вставки информации по фильмам.
+  const filmInputsEl = document.querySelector('.film-inputs');
+  const filmsInfo = new FilmsInfo(filmsTarget, filmsInfoEl);
+  console.log(filmsTarget);
+  changeMessage('Данные загружены.\n\n***');
+  inputStartBtn.style.display = 'none';
+  initSelects(filmsInfo.directors, filmsInfo.state);
+  filmInputsEl.style.display = 'block';
 };
 
-inputStartBtn.addEventListener('click', () => inputStart()); // По клику на кнопку запусе функции получния значения.
+// inputStartBtn.addEventListener('click', () => inputStart()); // По клику на кнопку запусе функции получния значения.
+inputStartBtn.addEventListener('click', () => {
+  changeMessage('Обновление данных...');
+  fetch('./films.json')
+    .then(response => response.json())
+    .then(loadSuccess)
+    .catch(changeMessage);
+});
